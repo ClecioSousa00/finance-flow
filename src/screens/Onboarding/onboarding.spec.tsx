a56300891044c31d76951fa167cf25fd522aebee
ClecioSousa00 <cleciosouza015@gmail.com>
2024-05-05 14:18:30 -0300

    add setup testing

---
+++ src/screens/Onboarding/onboarding.spec.tsx
@@ -0,0 +1,7 @@
+import { render } from '@testing-library/react-native'
+import { Onboarding } from '.'
+describe('render', () => {
+  it('render component', () => {
+    render(<Onboarding />)
+  })
+})
