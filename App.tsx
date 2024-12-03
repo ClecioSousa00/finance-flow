1548e33d84db0c51248ab429fa6b2d3b813b84f4
ClecioSousa00 <cleciosouza015@gmail.com>
2024-12-03 20:31:05 -0300

    Fix: splash screen

--- App.tsx
+++ App.tsx
@@ -1,3 +1,55 @@
+// import { GestureHandlerRootView } from 'react-native-gesture-handler'
+
+// import '@/styles/global.css'
+
+// import {
+//   useFonts,
+//   Poppins_400Regular,
+//   Poppins_500Medium,
+//   Poppins_600SemiBold,
+// } from '@expo-google-fonts/poppins'
+
+// import { useEffect } from 'react'
+// import { Appearance, SafeAreaView } from 'react-native'
+
+// import { StatusBar } from 'expo-status-bar'
+
+// import { Routes } from '@/routes'
+
+// import { UserProvider } from '@/contexts/userContext'
+// import { TransactionProvider } from '@/contexts/TransactionContext'
+
+// import Toast from 'react-native-toast-message'
+// import { colors } from '@/styles/colors'
+
+// export default function App() {
+//   const [fontsLoaded, fontError] = useFonts({
+//     Poppins_400Regular,
+//     Poppins_500Medium,
+//     Poppins_600SemiBold,
+//   })
+
+//   useEffect(() => Appearance.setColorScheme('light'), [])
+
+//   if (!fontsLoaded && !fontError) {
+//     return null
+//   }
+//   return (
+//     <SafeAreaView style={{ flex: 1, backgroundColor: colors.secondary }}>
+//       <StatusBar style="light" translucent />
+//       <GestureHandlerRootView
+//         style={{ flex: 1, backgroundColor: colors.secondary }}
+//       >
+//         <TransactionProvider>
+//           <UserProvider>
+//             <Routes />
+//             <Toast />
+//           </UserProvider>
+//         </TransactionProvider>
+//       </GestureHandlerRootView>
+//     </SafeAreaView>
+//   )
+// }
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 import '@/styles/global.css'
@@ -9,22 +61,26 @@ import {
   Poppins_600SemiBold,
 } from '@expo-google-fonts/poppins'
 
-import { useEffect } from 'react'
-import { Appearance, SafeAreaView } from 'react-native'
+import { useEffect, useState, useCallback } from 'react'
+import { SafeAreaView } from 'react-native'
 
 import { StatusBar } from 'expo-status-bar'
 
 import { Routes } from '@/routes'
 
-import { UserProvider, useUser } from '@/contexts/userContext'
+import { UserProvider } from '@/contexts/userContext'
+import { TransactionProvider } from '@/contexts/TransactionContext'
 
 import Toast from 'react-native-toast-message'
-import { TransactionProvider } from '@/contexts/TransactionContext'
 import { colors } from '@/styles/colors'
-import { Splash } from '@/screens/Splash'
+
+import * as SplashScreen from 'expo-splash-screen'
+import * as Font from 'expo-font'
+
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
-  const { loading } = useUser()
+  const [appIsReady, setAppIsReady] = useState(false)
 
   const [fontsLoaded, fontError] = useFonts({
     Poppins_400Regular,
@@ -32,13 +88,39 @@ export default function App() {
     Poppins_600SemiBold,
   })
 
-  useEffect(() => Appearance.setColorScheme('light'), [])
+  const onLayoutRootView = useCallback(() => {
+    if (appIsReady) {
+      SplashScreen.hide()
+    }
+  }, [appIsReady])
 
-  if ((!fontsLoaded && !fontError) || loading) {
-    return <Splash />
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync({
+          Poppins_400Regular,
+          Poppins_500Medium,
+          Poppins_600SemiBold,
+        })
+      } catch (e) {
+        console.warn(e)
+      } finally {
+        setAppIsReady(true)
+      }
+    }
+
+    prepare()
+  }, [])
+
+  if (!appIsReady || !fontsLoaded || fontError) {
+    return null
   }
+
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: colors.secondary }}>
+    <SafeAreaView
+      style={{ flex: 1, backgroundColor: colors.secondary }}
+      onLayout={onLayoutRootView}
+    >
       <StatusBar style="light" translucent />
       <GestureHandlerRootView
         style={{ flex: 1, backgroundColor: colors.secondary }}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,9 +4,25 @@ import { useUser } from '@/contexts/userContext'
 
 import { AuthRoute } from './auth.route'
 import { StackRoute } from './stack.route'
+import { Splash } from '@/screens/Splash'
+import { useEffect, useState } from 'react'
 
 export const Routes = () => {
-  const { user } = useUser()
+  const { user, loading } = useUser()
+
+  const [isSplashVisible, setIsSplashVisible] = useState(true)
+
+  useEffect(() => {
+    const splashTimeout = setTimeout(() => {
+      setIsSplashVisible(false)
+    }, 1500)
+
+    return () => clearTimeout(splashTimeout)
+  }, [])
+
+  if (loading || isSplashVisible) {
+    return <Splash />
+  }
 
   return (
     <NavigationContainer>
